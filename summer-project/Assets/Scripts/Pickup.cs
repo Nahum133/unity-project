93ca7b4960ecb26dd2b5b37671d218952e840116
Nahum133 <149899166+Nahum133@users.noreply.github.com>
2024-06-13 14:46:52 -0700

    I added a score and Winning Text

--- summer-project/Assets/Scripts/Pickup.cs
+++ summer-project/Assets/Scripts/Pickup.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     // Start is called before the first frame update
-    public int points;
     public float rotateSpeed = 5f; // A variable to changfe the rotation speed of the pickup
 
     void Start()
@@ -16,15 +15,12 @@ public class Pickup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    void OnTriggerEnter(Collider other){
-       if(other.gameObject.CompareTag("Player")){
-            points++;
-            Debug.Log(points);
-            Destroy(this.gameObject);
-       }
-       transform.Rotate(new Vector3(15, 30, 45) * rotateSpeed * Time.deltaTime);
+       transform.Rotate(new Vector3(15, 30, 45) * rotateSpeed * Time.deltaTime); 
     }
     
+    void OnTriggerEnter(Collider collider){
+        if(collider.gameObject.CompareTag("Player")){
+            GameManager.Instance.UpdateScore(1);
+        }
+    }
 }
